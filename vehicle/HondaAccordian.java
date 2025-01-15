5c5c8057ef33d6a30a77e4ef972d12fa1c1a9e1c
Jin Hong <happyhappyjinjin@gmail.com>
2025-01-15 09:26:31 -0500

    Completed HondaAccordian.java.

--- vehicle/HondaAccordian.java
+++ vehicle/HondaAccordian.java
@@ -2,10 +2,16 @@ package vehicle;
 
 public class HondaAccordian extends GasPoweredCar{
     /** modelYear specifies the year this car was made. Honda cares about that stuff. All Honda Accordian models have 14.5 gallon tanks and 33.2 MPG. */
-    public HondaAccordian(double startingOdometerValue, int modelYear)
+    public HondaAccordian(double startingOdometerValue, int modelYear) {
+        super("Honda", modelYear + "Accordian", startingOdometerValue, 33.2, 14.5);
+    }
     /** Defaults startingOdometerValue to 0. */
-    public HondaAccordian(int year)
+    public HondaAccordian(int year) {
+        this(0, year);
+    }
     /** Prints out the model year, make and model, and odometer miles. Ex: "2019 Honda Accordian (<miles> mi)"
     Coding tip: Write this method to re-use the behavior of the superclass toString. Don’t copy-and-paste the same code here. */
-    public String toString()
+    public String toString() {
+        return super.toString();
+    }
 }
