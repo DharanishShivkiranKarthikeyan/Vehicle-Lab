ff2e2309a8f4fa6a3b56b85bdae888d5eefcd997
Jin Hong <happyhappyjinjin@gmail.com>
2025-01-16 10:43:09 -0500

    format

--- vehicle/ChevroletBird.java
+++ vehicle/ChevroletBird.java
@@ -3,9 +3,13 @@ package vehicle;
 public class ChevroletBird extends ElectricCar implements Flying{
     /** Chevrolet Birds have a 250-mile range on a full charge. They
     start with their wings retracted. */
-    public ChevroletBird(double startingOdometerValue)
+    public ChevroletBird(double startingOdometerValue) {
+        
+    }
     /** Defaults startingOdometerValue to 0. */
-    public ChevroletBird()
+    public ChevroletBird() {
+
+    }
     /** Returns whether the wings are currently extended. */
     public boolean checkWingsExtended()
     /** Drives just like all other Electric Cars, except make sure that
